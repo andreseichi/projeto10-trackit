5cc3333fe09e357b5183069989d457425f6d457b
andreseichi <andre_seichi@hotmail.com>
2022-05-25 02:13:28 -0300

    renderizando os habitos da pagina hoje

--- src/pages/Hoje/index.jsx
+++ src/pages/Hoje/index.jsx
@@ -1,13 +1,67 @@
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import locale from 'dayjs/locale/pt-br';
+
+import { api } from '../../services/api';
+
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 
-import { Container } from './styles';
+import { TodayHabit } from '../../components/TodayHabit';
+
+import {
+  Container,
+  Content,
+  TodayHeader,
+  Title,
+  Text,
+  HabitsContainer,
+} from './styles';
 
 export function Hoje() {
+  const [todayHabits, setTodayHabits] = useState([]);
+
+  const data = dayjs().locale(locale).format('dddd, DD/MM');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const config = {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    };
+
+    api.get('habits/today', config).then((response) => {
+      if (response.status === 200) {
+        console.log(response);
+
+        const { data } = response;
+        setTodayHabits(data);
+      }
+    });
+  }, []);
+
   return (
     <Container>
       <Header />
-      <h1>hoje</h1>
+      <Content>
+        <TodayHeader>
+          <Title>{data}</Title>
+          <Text>Nenhum hábito concluído ainda</Text>
+        </TodayHeader>
+
+        <HabitsContainer>
+          {todayHabits?.map((habit) => (
+            <TodayHabit
+              key={habit.id}
+              name={habit.name}
+              done={habit.done}
+              currentSequence={habit.currentSequence}
+              record={habit.highestSequence}
+            />
+          ))}
+        </HabitsContainer>
+      </Content>
       <Footer />
     </Container>
   );
--- src/pages/Hoje/styles.js
+++ src/pages/Hoje/styles.js
@@ -6,3 +6,45 @@ export const Container = styled.div`
 
   background-color: #f2f2f2;
 `;
+
+export const Content = styled.section`
+  width: auto;
+  padding: 20px 18px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+export const TodayHeader = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const Title = styled.h2`
+  font-weight: 400;
+  font-size: 23px;
+  line-height: 29px;
+  color: #126ba5;
+
+  text-transform: capitalize;
+`;
+
+export const Text = styled.span`
+  font-size: 18px;
+  line-height: 22px;
+
+  color: #bababa;
+`;
+
+export const HabitsContainer = styled.div`
+  width: 100%;
+
+  margin-top: 28px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
