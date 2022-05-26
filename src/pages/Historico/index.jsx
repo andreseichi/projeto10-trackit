0794032b5ba34c280ed35a00f511a6f5333c3a30
andreseichi <andre_seichi@hotmail.com>
2022-05-25 21:17:27 -0300

    tela historico

--- src/pages/Historico/index.jsx
+++ src/pages/Historico/index.jsx
@@ -1,13 +1,17 @@
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 
-import { Container } from './styles';
+import { Container, Content, Title, Text } from './styles';
 
 export function Historico() {
   return (
     <Container>
       <Header />
-      <h1>historico</h1>
+      <Content>
+        <Title>Histórico</Title>
+
+        <Text>Em breve você poderá ver o histórico dos seus hábitos aqui!</Text>
+      </Content>
       <Footer />
     </Container>
   );
--- src/pages/Historico/styles.js
+++ src/pages/Historico/styles.js
@@ -6,3 +6,30 @@ export const Container = styled.div`
 
   background-color: #f2f2f2;
 `;
+
+export const Content = styled.section`
+  width: auto;
+  padding: 20px 18px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+export const Title = styled.h2`
+  font-weight: 400;
+  font-size: 23px;
+  line-height: 29px;
+  color: #126ba5;
+
+  text-transform: capitalize;
+`;
+
+export const Text = styled.span`
+  margin-top: 28px;
+
+  font-size: 18px;
+  line-height: 22px;
+
+  color: #666666;
+`;
